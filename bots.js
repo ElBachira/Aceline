a0f65d67d6ef0c204388aa67434a3f72d6f8ff37
ElBachira <bachirabastian@gmail.com>
2025-07-31 11:44:44 -0600

    Create bots.js

---
+++ bots.js
@@ -0,0 +1,25 @@
+// === TU MENÚ CENTRAL DE BOTS ===
+// ¡Este es el único archivo que necesitarás editar para añadir nuevos bots!
+
+const botsMasculinos = [
+    {
+        nombre: "Stellan",
+        imagen: "https://i.ibb.co/b3bwyXR/Stellan.png", // URL de la imagen del icono
+        link: "https://elbachira.github.io/Stellan/" // URL de la ficha completa
+    },
+    {
+        nombre: "Reuben",
+        imagen: "https://i.ibb.co/xxxx/reuben-icono.png", // ¡Reemplaza con la imagen real!
+        link: "https://elbachira.github.io/ficha-reuben/" // ¡Reemplaza con el link real!
+    },
+    // ... ¡Añade aquí tus otros bots masculinos!
+];
+
+const botsFemeninos = [
+     {
+        nombre: "Iris",
+        imagen: "https://i.ibb.co/xxxx/iris-icono.png", // ¡Reemplaza con la imagen real!
+        link: "https://elbachira.github.io/ficha-iris/" // ¡Reemplaza con el link real!
+    },
+    // ... ¡Añade aquí tus bots femeninos!
+];
